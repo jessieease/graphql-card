ac590cf094e756481b1a01d4ef55a12caa7d3a60
BO WU <bwuu0013@student.monash.edu>
2021-11-12 14:51:31 +1100

    replace the correct dir

--- src/components/SearchBox/SearchBox.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import SearchBox from './SearchBox';
-
-it('expected to render SearchBox Component', () => {
-  const mockPlaceholder = 'search by model';
-  const mockHandleChange = 'Canon'
-  expect(shallow(<SearchBox placeholder={mockPlaceholder} handleChange={mockHandleChange}/>)).toMatchSnapshot();
-})
